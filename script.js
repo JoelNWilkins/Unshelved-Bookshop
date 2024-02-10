9025165e632e5266572331d23bba20826ce0dbfb
wilkinsj2 <wilkinsj2@coventry.ac.uk>
2024-02-10 00:37:48 +0000

    Created 3D books which slide out of the bookshelf on hover and can be dragged into a stack at the side

--- script.js
+++ script.js
@@ -7,4 +7,67 @@ function rotateScrews() {
     }
 }
 
-window.onload = rotateScrews;
+function showAside() {
+    document.getElementById("aside").style.display = "block";
+}
+
+function hideAside() {
+    document.getElementById("aside").style.display = "none";
+}
+
+function toggleAside() {
+    let aside = document.getElementById("aside");
+    if (aside.style.display != "flex") {
+        aside.style.display = "flex";
+    } else {
+        aside.style.display = "none";
+    }
+}
+
+function allowDrop(event) {
+    if (event.target.id == "aside") {
+        event.preventDefault();
+    }
+}
+
+function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
+}
+
+function drop(event) {
+    event.preventDefault();
+    let data = event.dataTransfer.getData("text");
+    element = document.getElementById(data);
+    event.target.appendChild(element);
+    makePile();
+}
+
+function stackShelves() {
+    let sections = [...document.getElementsByTagName("section")];
+    for (var i=0; i < sections.length; i++) {
+        let books = sections[i].children;
+        position = 0;
+        for (var j=2; j < books.length; j++) {
+            books[j].style.left = position+"px";
+            position += books[j].offsetWidth;
+        }
+    }
+}
+
+function makePile() {
+    let aside = document.getElementById("aside");
+    let books = aside.children;
+    position = 0;
+    for (let book of books) {
+        book.style.left = (35 + (50 * Math.random()))+"px";
+        book.style.bottom = position+"px";
+        position += book.offsetWidth;
+    }
+}
+
+function onLoad() {
+    rotateScrews();
+    stackShelves();
+}
+
+window.onload = onLoad;
