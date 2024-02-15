ffc6afd942d6f1bf0becad49a8b4d40cc402c659
wilkinsj2 <wilkinsj2@coventry.ac.uk>
2024-02-15 02:34:58 +0000

    Made 3D stack from the bottom to the top

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function rotateScrews() {
     let screws = [...document.getElementsByClassName("screw")];
-    for (var i=0; i < screws.length; i++) {
+    for (let i=0; i < screws.length; i++) {
         let a = (Math.random() - 1) * 180;
         console.log("Rotating screw "+i+" "+a+" degrees");
         screws[i].style.transform = "rotate("+a+"deg)";
@@ -8,19 +8,25 @@ function rotateScrews() {
 }
 
 function showAside() {
-    document.getElementById("aside").style.display = "block";
+    document.getElementById("aside").style.display = "flex";
+    document.getElementById("main").style.width = "calc(100vw - 350px)";
 }
 
 function hideAside() {
     document.getElementById("aside").style.display = "none";
+    document.getElementById("main").style.width = "100vw";
 }
 
 function toggleAside() {
     let aside = document.getElementById("aside");
+    let main = document.getElementById("main");
     if (aside.style.display != "flex") {
         aside.style.display = "flex";
+        main.style.width = "calc(100vw - 350px)";
+        
     } else {
         aside.style.display = "none";
+        main.style.width = "100vw";
     }
 }
 
@@ -37,37 +43,39 @@ function drag(event) {
 function drop(event) {
     event.preventDefault();
     let data = event.dataTransfer.getData("text");
-    element = document.getElementById(data);
+    let element = document.getElementById(data);
+    element.style.left = (35 + (50 * Math.random()))+"px";
     event.target.appendChild(element);
-    makePile();
 }
 
 function stackShelves() {
     let sections = [...document.getElementsByTagName("section")];
-    for (var i=0; i < sections.length; i++) {
+    for (let i=0; i < sections.length; i++) {
         let books = sections[i].children;
-        position = 0;
-        for (var j=2; j < books.length; j++) {
+        let position = 0;
+        for (let j=2; j < books.length; j++) {
             books[j].style.left = position+"px";
             position += books[j].offsetWidth;
         }
     }
 }
 
-function makePile() {
-    let aside = document.getElementById("aside");
-    let books = aside.children;
-    position = 0;
-    for (let book of books) {
-        book.style.left = (35 + (50 * Math.random()))+"px";
-        book.style.bottom = position+"px";
-        position += book.offsetWidth;
-    }
-}
-
 function onLoad() {
     rotateScrews();
     stackShelves();
+    scrollResize();
+}
+
+function scrollResize() {
+    let scrollY = window.scrollY;
+    let aside = document.getElementById("aside");
+    let spacer = document.getElementById("spacer");
+    if (scrollY < 120) {
+        spacer.style.height = (160-scrollY)+"px";
+    } else {
+        spacer.style.height = "30px";
+    }
 }
 
 window.onload = onLoad;
+window.onscroll = scrollResize;
