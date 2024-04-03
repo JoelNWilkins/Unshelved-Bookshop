2a4cff2d5a40e3d29c11599b350a9aa7bcbf3bcd
wilkinsj2 <wilkinsj2@coventry.ac.uk>
2024-04-03 19:26:03 +0100

    Fixed CORS errors

--- functions/server.js
+++ functions/server.js
@@ -1,147 +1,130 @@
 
 const {onRequest} = require("firebase-functions/v2/https");
-// const express = require("express");
-// const cors = require("cors")({"Access-Control-Allow-Origin": "*"});
-// const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const {saveData, getData, getDataBatch} = require("./firebase.js");
 
 const SECRET_KEY = "your_secret_key";
 
-/*
-const app = express();
-
-// Allow cross-origin requests
-app.use(cors());
-
-// Serve static files from the "public" directory
-// app.use(express.static(path.join(__dirname, "public")));
-
-// Parse JSON requests
-app.use(bodyParser.json()); */
-
-const cors = {
-  cors: [
-    "http://localhost:5000/",
-    "https://us-central1-project-7097cem.cloudfunctions.net/",
-  ],
+const cors = (res) => {
+  res.set("Access-Control-Allow-Origin", "https://project-7097cem.web.app");
+  res.set("Access-Control-Allow-Methods", "GET, POST");
+  res.set("Access-Control-Allow-Headers", "*");
 };
 
-exports.books = onRequest(cors,
-    async (req, res) => {
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      console.log("GET /books");
-      const books = await getDataBatch("books");
-      res.status(200).send({data: books});
-    },
-);
+exports.books = onRequest(async (req, res) => {
+    console.log(`${req.method} /books`);
+    cors(res);
 
-exports.data = onRequest(cors,
-    async (req, res) => {
-      console.log(`GET ${req.url}`);
+    console.log("GET /books");
+    const books = await getDataBatch("books");
+    res.status(200).send({data: books});
+});
 
-      const [col, document, subdocument] = req.url.substring(1).split("/");
+exports.data = onRequest(async (req, res) => {
+    console.log(`${req.method} /data${req.url}`);
+    cors(res);
 
-      let text = "Col: "+col+", Document: "+document;
-      if (subdocument != null) {
-        text += ", Subdocument: "+subdocument;
-      }
-      console.log(text);
+    const [col, document, subdocument] = req.url.substring(1).split("/");
 
-      if (["authors", "genres"].includes(col)) {
-        let data = {};
-        if (document === "all") {
-          data = await getDataBatch(col);
+    let text = "Col: "+col+", Document: "+document;
+    if (subdocument != null) {
+      text += ", Subdocument: "+subdocument;
+    }
+    console.log(text);
+
+    if (["authors", "genres"].includes(col)) {
+      let data = {};
+      if (document === "all") {
+        data = await getDataBatch(col);
+      } else {
+        if (subdocument != null) {
+          data = await getData(col, document, "shelves", subdocument);
         } else {
-          if (subdocument != null) {
-            data = await getData(col, document, "shelves", subdocument);
-          } else {
-            data = await getData(col, document);
-            if (data?.shelves) {
-              const shelfData = {};
-              for (const shelf of data.shelves) {
-                if (shelf.includes("/")) {
-                  const parts = shelf.split("/");
-                  shelfData[shelf] = await getData(parts[0], parts[1]);
-                } else {
-                  const d = await getData(col, document, "shelves", shelf);
-                  shelfData[`${col}/${document}/${shelf}`] = d;
-                }
+          data = await getData(col, document);
+          if (data?.shelves) {
+            const shelfData = {};
+            for (const shelf of data.shelves) {
+              if (shelf.includes("/")) {
+                const parts = shelf.split("/");
+                shelfData[shelf] = await getData(parts[0], parts[1]);
+              } else {
+                const d = await getData(col, document, "shelves", shelf);
+                shelfData[`${col}/${document}/${shelf}`] = d;
               }
-              data.shelves = shelfData;
             }
+            data.shelves = shelfData;
           }
         }
-        return res.send({"data": data});
-      } else {
-        return res.status(401).send({error: "Unauthorized", data: null});
       }
-    },
-);
+      return res.send({"data": data});
+    } else {
+      return res.status(401).send({error: "Unauthorized", data: null});
+    }
+});
 
 // API endpoints for user CRUD operations
-exports.register = onRequest(cors,
-    async (req, res) => {
-      console.log(`POST ${req.url}`);
-      try {
-        const {username, password} = req.body;
+exports.register = onRequest(async (req, res) => {
+    console.log(`${req.method} /register${req.url}`);
+    cors(res);
 
-        // Hash the password before storing it
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const credentials = {"username": username, "password": hashedPassword};
-        const id = saveData("users", username, credentials);
-        if (id) {
-          // Create a JWT token for authentication
-          const token = jwt.sign({userId: id}, SECRET_KEY, {expiresIn: "1h"});
+    try {
+      const {username, password} = req.body;
+
+      // Hash the password before storing it
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const credentials = {"username": username, "password": hashedPassword};
+      const id = saveData("users", username, credentials);
+      if (id) {
+        // Create a JWT token for authentication
+        const token = jwt.sign({userId: id}, SECRET_KEY, {expiresIn: "1h"});
+        return res.send({data: token});
+      } else {
+        return res.status(500).send({
+          error: "Internal Server Error",
+          data: null,
+        });
+      }
+    } catch (err) {
+      console.log(err);
+      return res.status(500).send({
+        error: "Internal Server Error",
+        data: null,
+      });
+    }
+});
+
+exports.login = onRequest(async (req, res) => {
+    console.log(`${req.method} /login${req.url}`);
+    cors(res);
+
+    try {
+      const {username, password} = req.body;
+      const details = await getData("users", username);
+
+      if (details?.password !== undefined) {
+        const match = await bcrypt.compare(password, details?.password);
+        if (match) {
+          // Create a JWT token for authentication, {expiresIn: "1h"}
+          const token = jwt.sign({"username": username}, SECRET_KEY);
+
+          // Store the session in the database
+          saveData("sessions", null, {"user_id": username, "token": token});
           return res.send({data: token});
-        } else {
-          return res.status(500).send({
-            error: "Internal Server Error",
-            data: null,
-          });
         }
-      } catch (err) {
-        console.log(err);
-        return res.status(500).send({
-          error: "Internal Server Error",
-          data: null,
-        });
       }
-    },
-);
-
-exports.login = onRequest(cors,
-    async (req, res) => {
-      console.log(`POST ${req.url}`);
-      try {
-        const {username, password} = req.body;
-        const details = await getData("users", username);
-
-        if (details?.password !== undefined) {
-          const match = await bcrypt.compare(password, details?.password);
-          if (match) {
-            // Create a JWT token for authentication, {expiresIn: "1h"}
-            const token = jwt.sign({"username": username}, SECRET_KEY);
-
-            // Store the session in the database
-            saveData("sessions", null, {"user_id": username, "token": token});
-            return res.send({data: token});
-          }
-        }
-        return res.status(401).send({
-          error: "Invalid username or password",
-          data: null,
-        });
-      } catch (err) {
-        console.log(err);
-        return res.status(500).send({
-          error: "Internal Server Error",
-          data: null,
-        });
-      }
-    },
-);
+      return res.status(401).send({
+        error: "Invalid username or password",
+        data: null,
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).send({
+        error: "Internal Server Error",
+        data: null,
+      });
+    }
+});
 
 /*
 // Middleware to verify JWT token
@@ -166,11 +149,4 @@ const verifyToken = (req, res, next) => {
 app.get("/api/protected", verifyToken, (req, res) => {
   res.json({message: "This is a protected route"});
 });
-
-// Start the server
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
-
-exports.app = onRequest(app);
 */
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,23 +1,8 @@
-import { initializeApp } from "firebase/app";
-import { getFunctions, httpsCallable } from "firebase/functions";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDZBVCt1qKZnZV4P2f6_E0lkogL2Fphric",
-    authDomain: "project-7097cem.firebaseapp.com",
-    projectId: "project-7097cem",
-    storageBucket: "project-7097cem.appspot.com",
-    messagingSenderId: "396007541262",
-    appId: "1:396007541262:web:d120667ad3bf53ed3a0a5f",
-};
-  
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const functions = getFunctions(app);
-
 const formatEndpoint = (endpoint) => {
     const end = (endpoint[0] === "/") ? endpoint.stringify(1) : endpoint;
-    if (window.Location.hostname === "localhost") {
-        return "http://127.0.0.1:5000/project-7097cem/us-central1/"+end;
+    if (window.location.hostname === "localhost") {
+        return "http://127.0.0.1:5000/"+end;
+        //return "http://127.0.0.1:5000/project-7097cem/us-central1/"+end;
     } else {
         return "https://us-central1-project-7097cem.cloudfunctions.net/"+end;
     }
@@ -26,18 +11,33 @@ const formatEndpoint = (endpoint) => {
 async function getPublic(endpoint) {
     const response = await fetch(formatEndpoint(endpoint), {
         method: "GET",
-        headers: {crossorigin: true, "Content-Type": "application/json; charset=UTF-8"},
-    });
-    const data = await response.json();
-    console.log(data);
-    return data;
+        headers: new Headers({
+            crossorigin: true,
+            "Content-Type": "application/json; charset=UTF-8"
+        }),
+    })
+    .catch(error => console.log(error));
+
+    const result = await response.json();
+    console.log(result);
+    return result;
 }
 
-async function postData(endpoint, data) {
-    const callableResponse = httpsCallable(functions, endpoint);
-    return callableResponse(data)
-    .then(response => {console.log(response.data); data = response.data;})
-    .catch(error => {console.log(error); return null;});
+async function postData(endpoint, data, token=null) {
+    const response = await fetch(formatEndpoint(endpoint), {
+        method: "POST",
+        headers: new Headers({
+            crossorigin: true,
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json; charset=UTF-8"
+        }),
+        body: JSON.stringify(data)
+    })
+    .catch(error => console.log(error));
+
+    const result = await response.json();
+    console.log(result);
+    return result;
 }
 
 async function updateData(endpoint, token, data) {
