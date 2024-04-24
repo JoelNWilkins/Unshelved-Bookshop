425596eff0b01acd64d75095f5cfa0b599d05aca
wilkinsj2 <wilkinsj2@coventry.ac.uk>
2024-04-24 17:50:01 +0100

    Use a mixin for the transform-style, prevent some links from being blue on some browsers and import from firebase-admin since Cloud Functions are being used

--- functions/firebase.js
+++ functions/firebase.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
-const {initializeApp} = require("firebase/app");
+const {initializeApp} = require("firebase-admin/app");
+const {getFirestore} = require("firebase-admin/firestore");
 const {
-  getFirestore, collection, doc, // query, where,
+  collection, doc, // query, where,
   addDoc, setDoc, deleteDoc, updateDoc,
   getDoc, // getDocFromCache, getDocFromServer,
   getDocs, // getDocsFromCache, getDocsFromServer,
